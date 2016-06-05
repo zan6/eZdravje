f9e0cc008e69204061819bc852025d534c163d50
Zan Javornik <zj4421@student.uni-lj.si>
2016-06-05 08:35:00 +0000

    Popravljena opis in ideja aplikacije

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -327,7 +327,7 @@ function izracunajITM() {
 					    		}
 					    		else if(itm < 18.5){
 					    				$('#rezultatITM').html('<br/><span><b>Vaš ITM je </b>'+itm+
-					    				'in pove, da ste<b> podhranjeni.</b></span>'+
+					    				' in pove, da ste<b> podhranjeni.</b></span>'+
 					    				'<span>Za začetek, vam prilagam video,ki vam bo pomagal, da spremenite svoje prehranjevalne navade. </span>'+
 						    			'<br><br><iframe style="margin:5px" align:"top" src="https://www.youtube.com/embed/S9dfU99VvuQ" frameborder="0" allowfullscreen></iframe>');
 					    		}
@@ -338,12 +338,12 @@ function izracunajITM() {
 						    			'<br><br><iframe style="margin:5px" align:"top" src="https://www.youtube.com/embed/cIpo9_Fh8oE" frameborder="0" allowfullscreen></iframe>');
 					    		}else if(itm >= 30 && itm < 35){
 					    				$('#rezultatITM').html('<br/><span><b>Vaš ITM je </b>'+itm+
-					    				'in pove, da ste dosegli <b>1.stopnjo debelosti.</b></span>'+
+					    				' in pove, da ste dosegli <b>1.stopnjo debelosti.</b></span>'+
 					    				'Za začetek, vam prilagam video,ki vam bo pomagal pri izgubi odvečne maščobe. '+
 						    			'<br><br><iframe style="margin:5px" align:"top" src="https://www.youtube.com/embed/fcN37TxBE_s" frameborder="0" allowfullscreen></iframe>');
 					    		}else if(itm >= 35){
 					    			$('#rezultatITM').html('<br/><span>Vaš itm je '+itm+
-					    				'in pove, da ste dosegli 1.stopnjo debelosti.'+
+					    				' in pove, da ste dosegli 1.stopnjo debelosti.'+
 					    				' Odločili ste se, da želite spremeniti svoje navade. Odlično! Potrebna je le vztrajnost in uspelo vam bo! <b>Kjer je volja,tam je pot!</b>'+
 					    				'<span>Naj vam priložim  motivacijski video da boste videli da je z vztrajnostjo vse mogoče: </span>'+
 						    			'<br><br><iframe style="margin:5px" align:"top" src="https://www.youtube.com/embed/RvpIySenwfk" frameborder="0" allowfullscreen></iframe>'+
